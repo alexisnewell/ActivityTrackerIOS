331c6d07849fe5228b8cf028188739f5f5ac8f1a
Alexis Newell <alexisnewell05@gmail.com>
2026-07-27 21:24:56 -0700

    Initial Commit

---
+++ ActivityTrackerIOS/ActivityTrackerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerIOSApp.swift
+//  ActivityTrackerIOS
+//
+//  Created by Alexis Newell on 2026-07-27.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityTrackerIOSTests/ActivityTrackerIOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityTrackerIOSTests.swift
+//  ActivityTrackerIOSTests
+//
+//  Created by Alexis Newell on 2026-07-27.
+//
+
+import Testing
+
+struct ActivityTrackerIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
